4842b02704936aaf3e10dbcbe5f103fb5f136006
Riddhiman <shubro17@outlook.com>
2022-09-05 01:09:46 +0530

    otp error 2

--- routes/user.router.js
+++ routes/user.router.js
@@ -10,15 +10,15 @@ const usrschema = require('../model/user.mongo')
 const bcrypt = require('bcrypt')
 const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", // hostname
+    host: "smtp.office365.com", // hostname
     secureConnection: false, // TLS requires secureConnection to be false
     port: 587, // port for secure SMTP
     tls: {
        ciphers:'SSLv3'
     },
     auth: {
-        user: 'yorb999@gmail.com',
-        pass: 'yorb@))@'
+        user: 'shubro17@outlook.com',
+        pass: 'Yorb@))@'
     }
 });
 router.post('/register',async (req,res,next)=>{
